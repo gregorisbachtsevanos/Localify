57a0dda260cbc82fc699475344b12577c7f23675
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-03-26 17:14:36 +0200

    init service api

---
+++ server/src/main.ts
@@ -0,0 +1,29 @@
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix("api");
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  const port = configService.get<number>("PORT", 4000);
+  await app.listen(port);
+}
+
+void bootstrap();
